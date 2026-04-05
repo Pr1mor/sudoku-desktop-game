79f17fcf0c3be0d1651295bbc2b1383468c7d024
Pratham Patel <pratham.code10@gmail.com>
2026-04-05 01:01:25 -0500

    Changed the schemainitializer's path, now database is successfully initialized

--- JdbcDao/src/main/java/sudoku/jdbcdao/SchemaInitializer.java
+++ JdbcDao/src/main/java/sudoku/jdbcdao/SchemaInitializer.java
@@ -14,7 +14,7 @@ public class SchemaInitializer {
         try (Connection conn = DriverManager.getConnection(DB_URL);
              Statement stmt = conn.createStatement()) {
 
-            String sql = new String(Files.readAllBytes(Paths.get("src/main/database/schema.sql")));
+            String sql = new String(Files.readAllBytes(Paths.get("src/main/java/sudoku/jdbcdao/database/schema.sql")));
             stmt.executeUpdate(sql);
             System.out.println("Database schema initialized.");
         } catch (Exception e) {
